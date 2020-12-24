4d9aebe0a321ab7e272d315a520d35672b8f44fd
Emmy Yin <emmyy@kth.se>
2020-12-24 10:33:33 +0100

    extracting more info about each material

--- scrape_recycle_info.py
+++ scrape_recycle_info.py
@@ -3,20 +3,38 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 url = "https://www.stockholmvattenochavfall.se/avfall-och-atervinning/sortera-dina-sopor/sorteringsguiden/"
 
-data = []
+data = {}
+hazardous_materials = {}
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get(url)
 elems = driver.find_elements_by_class_name("waste-hit")
 
-counter = 0
+id = 0
 for item in elems:
-    attr = driver.execute_script('var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;', item)
-    name = item.find_element_by_tag_name("span").text
+    print("-----------------------")
 
-    # name = item.find_element_by_xpath("//span[@class='text item-name']").text
-    print(attr)
+    name = item.get_attribute("data-item-name")
+    synonyms = item.get_attribute("data-item-synonyms")
+    type = item.get_attribute("data-item-type")
+
+    info = item.find_element_by_xpath('//*[@class="toggle-target"]')
+    associated_hazardous_materials = info.find_elements_by_xpath('//*[@id="waste-hit-321"]/div/div[contains(@class ,"hazardous-material")]')
+    hazardous_materials = []
+    for material in associated_hazardous_materials:
+        hazardous_materials.append(material.get_attribute("title"))
+
+    recycle_place = info.find_elements_by_xpath('//*[@id="waste-hit-321"]/div/div[@class="table"]/div[@data-show-for-private="true"]/div[@class="table-cell wiki-search-info"]/*')
+    for item in recycle_place:
+        attr = driver.execute_script('var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;', item)
+        print(attr)
+
+    data.update({id: {"name": name, "synonyms": synonyms, "type": type, "hazardous_materials": hazardous_materials}})
+    print(name, synonyms, type, hazardous_materials)
+
+    id += 1
 
 print(len(elems))
+# print(data)
 
 driver.quit()
