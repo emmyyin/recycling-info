d4fb31c67d1dca5e845f640b97cf121a940f8c30
Emmy Yin <emmy.yins@gmail.com>
2021-01-17 22:11:15 +0100

    testing

--- recycle_app/lib/main.dart
+++ recycle_app/lib/main.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:recycle_app/db_provider.dart';
+import 'package:recycle_app/models/hazardous_material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold()
-    );
+    return MaterialApp(title: 'Flutter Demo', 
+    home: Scaffold(
+      body: FutureBuilder<List<HazardousMaterial>>(
+        future: DBProvider.db.getAllHazardousMataerials(),
+        builder: (BuildContext context, AsyncSnapshot<List<HazardousMaterial>> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                HazardousMaterial item = snapshot.data[index];
+                return ListTile(
+                  title: Text(item.name),
+                  leading: Text(item.id.toString()),
+                );
+              },
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    ),);
   }
 }
-
