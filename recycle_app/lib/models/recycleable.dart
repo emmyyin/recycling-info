be2efda4bc0847a7d33fd2a7d7d1bb51c4294b9a
Emmy Yin <emmy.yins@gmail.com>
2021-01-28 21:26:23 +0100

    updated model

--- recycle_app/lib/models/recycleable.dart
+++ recycle_app/lib/models/recycleable.dart
@@ -1,13 +1,11 @@
+import 'package:flutter/material.dart';
+
 class Recycleable {
-  String id;
+  String id = "";
   String type;
   List<String> names;
-  List<String> hazardous;
-  List<String> recyclePlaces;
+  List<String> hazardous = [];
+  List<String> recyclePlaces = [];
 
-  Recycleable({this.id, this.type, this.names, this.hazardous, this.recyclePlaces});
-
-  Map<String, dynamic> toMap() {
-    return {'id': id, 'type': type, 'names': names, 'hazardous': hazardous, 'recycle_places': recyclePlaces};
-  }
+  Recycleable({this.id, @required this.type, @required this.names, this.hazardous, this.recyclePlaces});
 }
