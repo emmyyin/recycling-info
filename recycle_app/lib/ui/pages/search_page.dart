bdb48d7883cea0cf0cedd78ac70ebde266ec5f9b
Emmy Yin <emmy.yins@gmail.com>
2021-02-03 09:49:26 +0100

    list widget

--- recycle_app/lib/ui/pages/search_page.dart
+++ recycle_app/lib/ui/pages/search_page.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
 
+import 'package:recycle_app/resources/database.dart';
+import 'package:recycle_app/ui/widgets/dynamic_list.dart';
+
 class SearchPage extends StatefulWidget {
   @override
   _SearchPageState createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> {
-  var _searchEdit = new TextEditingController();
-  bool _isSearch = true;
-  String _searchText = "";
+  DBProvider _db = DBProvider();
+  List<dynamic> _list;
 
-  List<String> _recycleableItems;
-  List<String> _searchedItems;
+  @override
+  void initState() {
+    getList().then((list) {
+            setState(() {
+                _list = list;
+            });
+        });
+    getList();
+  }
+
+  Future<List<dynamic>> getList() async {
+    return await _db.getRecycleables();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (_list.isEmpty){
+      return CircularProgressIndicator();
+    }
+
+    return Container(
+      child: DynamicList(_list),
+    );
   }
 }
---
+++ recycle_app/lib/ui/widgets/dynamic_list.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:recycle_app/models/recycleable.dart';
+
+class DynamicList extends StatelessWidget {
+  List<dynamic> list;
+
+  DynamicList(this.list);
+
+  Card _recycleableCard(Recycleable r) {
+    return Card(
+      child: ListTile(
+        title: Text(r.names[0]),
+      ),
+    );
+  }
+
+  Widget _myListView(BuildContext context) {
+    return ListView.builder(
+      itemCount: list.length,
+      itemBuilder: (context, index) {
+        return _recycleableCard(list[index]);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: _myListView(context),
+    );
+  }
+}
