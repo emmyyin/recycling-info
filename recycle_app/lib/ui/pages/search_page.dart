8f3cd8778d68930b5a9bceb520ee7300816d13af
Emmy Yin <emmy.yins@gmail.com>
2021-02-03 22:08:51 +0100

    added search function

--- recycle_app/lib/ui/pages/search_page.dart
+++ recycle_app/lib/ui/pages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recycle_app/models/recycleable.dart';
 
 import 'package:recycle_app/resources/database.dart';
 import 'package:recycle_app/ui/widgets/dynamic_list.dart';
@@ -9,31 +10,59 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  TextEditingController _textController = TextEditingController();
   DBProvider _db = DBProvider();
-  List<dynamic> _list;
+  List<dynamic> _list = [];
+  List<dynamic> _searchList = [];
 
   @override
   void initState() {
-    getList().then((list) {
-            setState(() {
-                _list = list;
-            });
-        });
-    getList();
+    _getList().then((list) {
+      setState(() {
+        _list = list;
+        _searchList = list;
+      });
+    });
   }
 
-  Future<List<dynamic>> getList() async {
+  Future<List<dynamic>> _getList() async {
     return await _db.getRecycleables();
+  }
+
+  void _onItemChanged(String value) {
+    setState(() {
+      List<dynamic> newList = [];
+      for (Recycleable r in _list) {
+        for (String name in r.names) {
+          if (name.toLowerCase().contains(value.toLowerCase())) {
+            newList.add(r);
+            break;
+          }
+        }
+      }
+      _searchList = newList;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_list.isEmpty){
-      return CircularProgressIndicator();
-    }
-
-    return Container(
-      child: DynamicList(_list),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 16.0),
+          child: TextField(
+            controller: _textController,
+            decoration: InputDecoration(
+              hintText: 'Sök'
+            ),
+            textAlign: TextAlign.center,
+            onChanged: _onItemChanged,
+          ),
+        ),
+        _list.isEmpty
+            ? CircularProgressIndicator()
+            : Expanded(child: DynamicList(_searchList))
+      ],
     );
   }
 }
