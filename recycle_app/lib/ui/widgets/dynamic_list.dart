375cb6d2acf7dc8bfc11b8c28159a0c9b635d6bf
Emmy Yin <emmy.yins@gmail.com>
2021-02-03 11:12:38 +0100

    fixing list

--- recycle_app/lib/ui/widgets/dynamic_list.dart
+++ recycle_app/lib/ui/widgets/dynamic_list.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 
 import 'package:recycle_app/models/recycleable.dart';
+import 'package:recycle_app/ui/pages/info_page.dart';
 
+/// Display a dynamic list of all the recycleables
 class DynamicList extends StatelessWidget {
   List<dynamic> list;
 
   DynamicList(this.list);
 
-  Card _recycleableCard(Recycleable r) {
+  Card _recycleableCard(BuildContext context, Recycleable r) {
     return Card(
       child: ListTile(
         title: Text(r.names[0]),
         trailing: Icon(Icons.arrow_right),
+        onTap: () {
+          Route route = MaterialPageRoute(builder: (context) => InfoPage(r));
+          Navigator.push(context, route);
+        },
       ),
     );
   }
@@ -20,7 +26,7 @@ class DynamicList extends StatelessWidget {
     return ListView.builder(
       itemCount: list.length,
       itemBuilder: (context, index) {
-        return _recycleableCard(list[index]);
+        return _recycleableCard(context, list[index]);
       },
     );
   }
